1fd093254d2660530693495b8400231d50f2ab48
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-11 12:35:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+// Your code here //
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) { // To ensure the dodger doesn't move off the screen to the left
+      dodger.style.left = `${left - 1}px`; // Move 1px to the left
+    }
+  }
+  
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    // Assuming a boundary of 400px for the right edge. Adjust as needed.
+    if (left < 400) { // To ensure the dodger doesn't move off the screen to the right
+      dodger.style.left = `${left + 1}px`; // Move 1px to the right
+    }
+  }
+  
